957c6ccd0c3cd9a25abc9c0b349861b45125187b
Allison Emily Coleman <allison@parseceducation.com>
2024-04-08 19:42:12 -0700

    feat: read console input

--- investment_calulator.go
+++ investment_calulator.go
@@ -6,8 +6,17 @@ import (
 )
 
 func main() {
-	investmentAmount, years, expectedReturnRate := 1000.0, 10.0, 5.5
+	// I was suggested to use 2.0% - as if!
+	const inflationRate = 3.2
+	var investmentAmount float64
+	years, expectedReturnRate := 10.0, 5.5
+
+	// Store the results of fmt.Scan into the value at the adress of investmentAmount
+	fmt.Scan(&investmentAmount)
 
 	futureValue := investmentAmount * math.Pow(1+expectedReturnRate/100, years)
+	futureInflatedValue := futureValue / math.Pow(1+inflationRate/100, years)
+
 	fmt.Println(futureValue)
+	fmt.Println(futureInflatedValue)
 }
