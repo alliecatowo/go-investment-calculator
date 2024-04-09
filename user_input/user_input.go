9cebbab03ed91616afa80a2982220b07049f69fb
Allison Emily Coleman <allison@parseceducation.com>
2024-04-09 09:41:43 -0700

    feat: user input package

---
+++ user_input/user_input.go
@@ -0,0 +1,12 @@
+package user_input
+
+import "fmt"
+
+func GetInput(prompt string) (input float64) {
+	fmt.Println(prompt)
+
+	// Store the results of fmt.Scan into the value at the address of `input`
+	fmt.Scan(&input)
+
+	return input
+}
