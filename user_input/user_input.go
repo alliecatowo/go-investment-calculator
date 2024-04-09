1130020d42e0011a48d257e1b1d138a37ed373d6
Allison Emily Coleman <allison@parseceducation.com>
2024-04-09 10:23:00 -0700

    feat: prompt on the same line

--- user_input/user_input.go
+++ user_input/user_input.go
@@ -3,8 +3,7 @@ package user_input
 import "fmt"
 
 func GetInput(prompt string) (input float64) {
-	fmt.Println(prompt)
-
+	fmt.Print(prompt)
 	// Store the results of fmt.Scan into the value at the address of `input`
 	fmt.Scan(&input)
 
