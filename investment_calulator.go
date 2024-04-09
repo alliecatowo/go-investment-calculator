d3cdfdcb6d7bfb6f9ac76b9aef6cd45260667eb1
Allison Emily Coleman <allison@parseceducation.com>
2024-04-08 19:45:51 -0700

    feat: add user prompts

--- investment_calulator.go
+++ investment_calulator.go
@@ -11,12 +11,19 @@ func main() {
 	var investmentAmount float64
 	years, expectedReturnRate := 10.0, 5.5
 
+	fmt.Println("Enter the amount you want to invest: ")
 	// Store the results of fmt.Scan into the value at the adress of investmentAmount
 	fmt.Scan(&investmentAmount)
+
+	fmt.Println("Enter how many years you want to invest for: ")
+	fmt.Scan(&years)
+
+	fmt.Println("Enter the expected return rate: ")
+	fmt.Scan(&expectedReturnRate)
 
 	futureValue := investmentAmount * math.Pow(1+expectedReturnRate/100, years)
 	futureInflatedValue := futureValue / math.Pow(1+inflationRate/100, years)
 
-	fmt.Println(futureValue)
-	fmt.Println(futureInflatedValue)
+	fmt.Println("Your expected value after", years, "years is $", futureValue)
+	fmt.Println("Accounting for a", inflationRate, "% inflation rate, your expected value after", years, "years is $", futureInflatedValue)
 }
