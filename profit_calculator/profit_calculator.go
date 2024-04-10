75264991a06f7c06fac895cbaf810a235e7a837d
Allison Emily Coleman <allison@parseceducation.com>
2024-04-10 14:20:52 -0700

    feat: write profit functionality

--- profit_calculator/profit_calculator.go
+++ profit_calculator/profit_calculator.go
@@ -2,9 +2,37 @@ package profit_calculator
 
 import (
 	"fmt"
-
+	"os"
+	"strconv"
+	"errors"
 	"github.com/alliecatowo/go-investment-calculator/user_input"
 )
+
+func writeProfit(profit float64) {
+	profitBytes := []byte(fmt.Sprintf("%.2f", profit))
+	err := os.WriteFile("profit.txt", profitBytes, 0644)
+	if err != nil {
+		panic("Can't write profit to file! Aborting application!")
+	}
+}
+
+func getProfit() (float64, error) {
+	data, err := os.ReadFile("profit.txt")
+
+	if err != nil {
+		writeProfit(100.0)
+		return 100.0, errors.New("profit file not found. Default profit of $100.00 set")
+	}
+
+	profitText := string(data)
+	profit, err := strconv.ParseFloat(profitText, 64)
+
+	if err != nil {
+		writeProfit(100.0)
+		return 100.0, errors.New("failed to parse profit. Default profit of $100.00 set")
+	}
+	return profit, nil
+}
 
 func Prompt() {
 	revenue := user_input.GetInput("Enter Your Revenue:")
